d2c37d309defca61fab8dad645c8f21ac5d1a8c1
Geovani Castro <geocas47@cs.washington.edu>
2019-01-13 16:12:49 -0800

    Tentatively finished ListFIFOQueue.

--- src/datastructures/worklists/ListFIFOQueue.java
+++ src/datastructures/worklists/ListFIFOQueue.java
@@ -1,5 +1,7 @@
 package datastructures.worklists;
 
+import java.util.NoSuchElementException;
+
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.FIFOWorkList;
 
@@ -8,33 +10,70 @@ import cse332.interfaces.worklists.FIFOWorkList;
  * for method specifications.
  */
 public class ListFIFOQueue<E> extends FIFOWorkList<E> {
+	Node front;
+	Node back;
+	int size;
+	
+	private class Node {
+		E val;
+		Node next; 
+		
+		public Node() {
+			next = new Node(null);
+		}
+		
+		public Node(E val) {
+			this.val = val;
+		}
+	}
     
     public ListFIFOQueue() {
-        throw new NotYetImplementedException();
+        setup();
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+    	if  (isEmpty()) {
+    		front = new Node(work);
+    		back = front;
+    	}
+        back.next = new Node(work);
+        back = back.next;
+        size++;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (isEmpty()) {
+        	throw new NoSuchElementException();
+        }
+        return front.val;
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        E val = front.val;
+        front = front.next;
+        return val;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        setup();
+    }
+    
+    private boolean isEmpty() {
+    	return front == null;
+    }
+    
+    private void setup() {
+    	front = new Node();
+        back = null;
+        size = 0;
     }
 }
